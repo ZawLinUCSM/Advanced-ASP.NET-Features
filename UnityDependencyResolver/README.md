25275173be9791fcc8ecc4d05bb85d6ebc88388f
ZawLinUCSM <53468778+ZawLinUCSM@users.noreply.github.com>
2022-08-14 13:59:55 +0700

    Added project for Unity Dependency Resolver

---
+++ UnityDependencyResolver/README.md
@@ -0,0 +1,12 @@
+# Advanced-ASP.NET-Features
+Using the Unity Dependency Resolver
+# Steps to implement
+ Step 1: Instal nuget package ("Unity")
+ Step 2: Add a new class based on IDependency Resolver (UnityResolver : IDependencyResolver)
+ Step 3: Add Unity Container and Services in WebApiConfig.cs
+ Step 4: Add DI in Controller 
+ Step 5: Test.
+
+# Endpoint to test
+http://localhost:55319/api/students
+
---
+++ UnityDependencyResolver/WebApi.DI/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Unity;
+using Unity.Lifetime;
+using WebApi.DI.Data;
+using WebApi.DI.Data.DependencyResolver;
+
+namespace WebApi.DI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            //Configure Unity Container
+            var container = new UnityContainer();
+            container.RegisterType<IStudentRepository, StudentRepository>(new PerResolveLifetimeManager());
+            config.DependencyResolver = new UnityResolver(container);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
